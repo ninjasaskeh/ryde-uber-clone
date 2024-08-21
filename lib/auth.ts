97cc9e023339743db5ad6bc31b1fe2048d4d7031
ninjasaskeh <77367354+ninjasaskeh@users.noreply.github.com>
2024-08-21 22:07:16 +0700

    Uber Clone
    CIHUUUYYY

---
+++ lib/auth.ts
@@ -0,0 +1,75 @@
+import * as SecureStore from "expo-secure-store";
+import * as Linking from "expo-linking";
+import { fetchAPI } from "@/lib/fetch";
+
+export const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+export const googleOAuth = async (startOAuthFlow: any) => {
+  try {
+    const { createdSessionId, signUp, setActive } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(root)/(tabs)/home", {
+        scheme: "myapp",
+      }),
+    });
+
+    if (createdSessionId) {
+      if (setActive) {
+        await setActive!({ session: createdSessionId });
+
+        if (signUp.createdUserId) {
+          await fetchAPI("/(api)/user", {
+            method: "POST",
+            body: JSON.stringify({
+              name: `${signUp.fieldName} ${signUp.lastName}`,
+              email: signUp.email,
+              clerkId: signUp.createdUserId,
+            }),
+          });
+        }
+
+        return {
+          success: true,
+          code: "success",
+          message: "You have successfully authenticated with Google",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      code: "success",
+      message: "An error occurred.",
+    };
+  } catch (error: any) {
+    console.log(error);
+
+    return {
+      success: false,
+      error: error.code,
+      message: error?.error[0]?.longMessage,
+    };
+  }
+};
